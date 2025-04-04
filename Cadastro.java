edcb80ac4c34ded01f70b4ed917e1f7049ed6110
Higor <higor.e.silva6@aluno.senai.br>
2025-04-04 10:58:30 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,33 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String studentName = "Higox Silva";
+    int studentID = 18;
+    int studentAge = 16;
+    float studentFee = 98.00f;
+    String studentGrade = "1DSSeduc";
+    
+    
+    // Print variables
+    System.out.println("student Name:" + studentName);
+    System.out.println("student ID:" + studentID);
+    System.out.println("student Age:" + studentAge);
+    System.out.println("student Fee:" + studentFee);
+    System.out.println("student Grade:" + studentGrade);
+    }
+    
+}
---
+++ Comandos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comandos;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Comandos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print ("Seja bem vindo!");
+        System.out.print ("Higox");
+        System.out.println ("tudo bem?");
+        System.out.println ("Como esta sendo seu dia?");
+        System.out.println ("Que seu dia seja bom");
+        System.out.println (3);
+        System.out.println (358);
+        System.out.println (50000);
+        System.out.println (3 * 6);
+        System.out.println (2 + 8);
+        String name= "Higox";
+        System.out.println ("Oi " + name);
+        int myNum = 26;
+        System.out.println(myNum);
+        int AnoAtual = 2025;
+        int AnoNascimento = 2009;
+        System.out.println (AnoAtual-AnoNascimento);
+        String FirstName = "Higox ";
+        String LastName = "Silva";
+        String FullName = FirstName + LastName;
+        System.out.println (FullName);
+        int x = 5, y = 5, z = 5;
+        System.out.println(x + y + x);
+        int a, b, c;
+        a = b = c = 50;
+         System.out.println(a + b + c);
+        //
+    }
+    
+}
